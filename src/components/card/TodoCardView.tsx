924e845558c14f528abd8628df2b298199521bae
Oleksandr ZANIN <oz@nemesiscorporation.com>
2026-02-24 15:53:46 +0200

    show search highlights

--- src/components/card/TodoCardView.tsx
+++ src/components/card/TodoCardView.tsx
@@ -1,4 +1,6 @@
 import type { Task } from "../../models";
+import { useBoard } from "../../hooks";
+import type { JSX } from "react";
 
 interface TodoCardViewProps {
   isCompleted?: boolean;
@@ -10,6 +12,37 @@ export function TodoCardView({
   onChange,
   item,
 }: TodoCardViewProps) {
+  const { state } = useBoard();
+
+  const getTitleWithSearchMatches = () => {
+    const searchRegex = new RegExp(state.searchQuery, "i");
+    const matches = item.title.match(searchRegex);
+
+    const matchedSubstring = matches?.[0];
+
+    if (matchedSubstring) {
+      const remainingParts = item.title.split(searchRegex);
+
+      return remainingParts.reduce(
+        (acc: (string | JSX.Element)[], part, index) => {
+          acc.push(part);
+
+          if (index < remainingParts.length - 1) {
+            acc.push(
+              <span key={index} className="bg-yellow-200">
+                {matchedSubstring}
+              </span>,
+            );
+          }
+          return acc;
+        },
+        [],
+      );
+    }
+
+    return item.title;
+  };
+
   return (
     <div className="flex items-start gap-2">
       <input
@@ -19,7 +52,7 @@ export function TodoCardView({
         onChange={onChange}
       />
       <p className={`${isCompleted ? "line-through text-slate-500" : ""}`}>
-        {item.title}
+        {getTitleWithSearchMatches()}
       </p>
     </div>
   );
