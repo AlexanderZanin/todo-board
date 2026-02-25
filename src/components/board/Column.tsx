91ec981e2b5e06878f448c90c952bf2d41734ba2
Oleksandr ZANIN <oz@nemesiscorporation.com>
2026-02-25 14:25:59 +0200

    change column width for different screens

--- src/components/board/Column.tsx
+++ src/components/board/Column.tsx
@@ -1,6 +1,6 @@
 export function Column({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-80 flex-shrink-0 bg-slate-100 rounded-lg flex flex-col">
+    <div className="w-56 sm:w-64 md:w-80 shrink-0 bg-slate-100 rounded-lg flex flex-col">
       {children}
     </div>
   );
